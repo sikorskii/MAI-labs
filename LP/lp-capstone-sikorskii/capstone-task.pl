b37bc58cce43ff63a480ea39167a6cb06b89ab1d
sikorskii <rokmac96@gmail.com>
2021-11-21 20:57:01 +0300

    prolog capstone

--- LP/lp-capstone-sikorskii/capstone-task.pl
+++
@@ -1,9 +0,0 @@
-:- ['dataFIle.pl'].
-
-% Реализовать предикат проверки/поиска тещи
-
-
-
-mother_in_law(M, X) :- male(M), child(C, M), findall(Z, child(C, Z), [H,T]), findall(L, child(T, L), [U,X]).
-
-?- mother_in_law('Сокет Шарпов', X).
